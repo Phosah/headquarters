3d30133be3e9ed4527e1e1cb86e2f52c59529128
Phosah <efosa.uyi@gmail.com>
2023-01-22 22:18:44 +0100

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Todos</h1>
+    <p>No todos currently</p>
+  </main>
+</template>
---
+++ src/views/Review.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Reviews">
+    <h1>This is a review page</h1>
+    <p>No reviews yet</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
